4db308cbca88bc84f98aa17333ad0dab95b010eb
Miheretab Mengesha <mengeshamiheretab36@gmail.com>
2024-11-07 19:31:49 +0300

    updateFinal

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,12 @@
+import spinner from '../assets/spinner.gif'
+
+const Spinner = () => {
+  return <img src={Spinner} alt='Loading...'
+  style={{width: '100px', margin: 'auto',
+    display: 'block'
+  }}
+  />
+
+}
+
+export default Spinner
